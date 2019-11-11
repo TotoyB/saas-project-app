40944ac58067066ee6af13cc0cafd3aac35e5ee0
TotoyB <tadeoalwilson.pfs@gmmail.com>
2019-11-11 11:45:13 +0800

    S11 eps301

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,25 @@
 Rails.application.routes.draw do
-  root 'home#index'
+
+  resources :tenants do
+    resources :projects
+  end
+  resources :members
+  get 'home/index'
+  root :to => "home#index"
+    
+  # *MUST* come *BEFORE* devise's definitions (below)
+  as :user do
+    match 'user/confirmation' => 'confirmations#update', :via => :put, :as => :update_user_confirmation
+  end
+
+  devise_for :users, :controllers => { 
+    :registrations => "milia/registrations",
+    :confirmations => "confirmations",
+    :sessions => "milia/sessions", 
+    :passwords => "milia/passwords", 
+  }
+
+
+  #root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
